4a4bd413b258f70a2edc5a4cec8b90e56fdeb356
stevenrhenaldy <stevenrhenaldy@gmail.com>
2022-12-08 22:27:41 +0800

    card and account

--- App/Models/Account.php
+++ App/Models/Account.php
@@ -1,4 +1,5 @@
 <?php
+namespace Models;
 
 enum Type{
     case Business;
@@ -9,6 +10,21 @@ class Accounts{
     private string $account_number;
     private int $amount;
     private Type $type;
+    private Card $cards;
     
+    public function __construct(){
+        $this->cards = array();
+    }
     
+    public function setAccountNumber($account_number){
+        $this->account_number = $account_number;
+    }
+
+    public function setAmount($amount){
+        $this->amount = $amount;
+    }
+
+    public function setType($type){
+        $this->type = $type;
+    }
 }
--- App/Models/Card.php
+++ App/Models/Card.php
@@ -1,21 +1,39 @@
 <?php
 namespace Models;
 
-// No need to state inside the ppt this class :)
-// I added this class as PHP don't have date datatype built in
-class MonthYear{
-    private int $month;
-    private int $year;
-}
-// ********************************************
-
 class Card{
     private string $card_number;
-    private int $expiry_year;
-    private MonthYear $month_year;
-    private int $cvv;
+    private int $expiry_year;      //Timestamp
+    private string $cvv;
 
-    public function setCardNumber($cardNumber): void{
-        
+    public function setCardNumber($card_number): void{
+        $this->card_number = $card_number;
+    }
+
+    public function setexpiryYear($expiry_year): void{
+        $this->expiry_year = $expiry_year;
+    }
+
+    public function setCVV($cvv): void{
+        $this->cvv = $cvv;
+    }
+
+    public function getCardNumber(): string{
+        return $this->card_number;
+    }
+
+    public function getExpiryYear(): int{
+        return $this->expiry_year;
+    }
+
+    public function getCVV(): string{
+        return $this->cvv;
+    }
+
+    public function authenticateCVV($cvv): bool{
+        if($this->cvv == $cvv){
+            return true;
+        }
+        return false;
     }
 }
