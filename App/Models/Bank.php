c86c71cafc564637100299c11051829f4b965457
stevenrhenaldy <stevenrhenaldy@gmail.com>
2022-12-09 08:26:22 +0800

    update

--- App/Models/Bank.php
+++ App/Models/Bank.php
@@ -2,12 +2,51 @@
 namespace Models;
 
 class Bank{
-    private User $users;
+    private $users;
     private string $name;
     
     public function __construct(string $bankName){
         $this->name = $bankName;
         $users = array();
+    }
+
+    public function addUser(User $user){
+        array_push($this->users,$user);
+    }
+
+    public function getAllUsers(){
+        return $this->users;
+    }
+
+    public function getUser($id){
+        if(!is_null($id)){
+            return $this->users[$id];
+        }
+    }
+
+    public function searchUser(?string $username = null, ?Authorization $authorization = null, ?string $email = null){
+        // fulltext search
+        $result = array();
+        foreach ($this->users as $u) {
+            if(!is_null($username)){
+                if ($u->getusername() !== $username) {
+                    continue;
+                }
+            }
+            if(!is_null($authorization)){
+                if ($u->getAuthorization() !== $authorization) {
+                    continue;
+                }
+            }
+            if(!is_null($email)){
+                if ($u->getEmail() !== $email) {
+                    continue;
+                }
+            }
+            array_push($result, $u);
+        }
+
+        return $result;
     }
 
 }
--- App/Models/Staff.php
+++ App/Models/Staff.php
@@ -1,6 +1,28 @@
 <?php
 namespace Models;
 
-class Staff{
-    
+enum Division{
+    case General_Manager;
+    case Service_Center;
+}
+
+class Staff extends User{
+    private Division $division;
+    private string $extention;
+
+    public function setDivision(Division $division){
+        $this->division = $division;
+    }
+
+    public function getDivision(){
+        return $this->division;
+    }
+
+    public function setExtention(string $extention){
+        $this->extention = $extention;
+    }
+
+    public function getExtention(){
+        return $this->extention;
+    }
 }
